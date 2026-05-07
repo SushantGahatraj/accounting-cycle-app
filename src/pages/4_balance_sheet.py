f0521304141f23e0394655e07c46c6142aebab62
Sushant <sushantgahatraj2023@gmail.com>
2026-05-07 08:47:42 -0400

    same as other pages started adding functions

--- src/pages/4_balance_sheet.py
+++ src/pages/4_balance_sheet.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 
 #Path helper
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
+def get_data_path(filename: str) -> str:
+    '''Returns the path to a data file given its filename.'''
+    return os.path.join(APP_PATH, "..", "data", filename)
